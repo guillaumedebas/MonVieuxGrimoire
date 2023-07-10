b22d3bea494d756f5e2756a341ad10e48fe303d3
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-07-10 09:05:02 +0200

    Added check for existing user in signup controller

--- controllers/user.js
+++ controllers/user.js
@@ -4,18 +4,28 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({message:'User created'}))
-        .catch(error => res.status(400).json({error}));
-    })
-    .catch(error => res.status(500).json({error}));
-};
+    const { email, password } = req.body;
+  
+    User.findOne({ email: email })
+      .then((existingUser) => {
+        if (existingUser) {
+          return res.status(409).json({ error: 'User already exists' });
+        }
+  
+        bcrypt.hash(password, 10)
+          .then((hash) => {
+            const user = new User({
+              email: email,
+              password: hash
+            });
+            user.save()
+              .then(() => res.status(201).json({ message: 'User created' }))
+              .catch((error) => res.status(400).json({ error }));
+          })
+          .catch((error) => res.status(500).json({ error }));
+      })
+      .catch((error) => res.status(500).json({ error }));
+  };
 
 exports.login = (req, res, next) => {
     User.findOne({email: req.body.email})
