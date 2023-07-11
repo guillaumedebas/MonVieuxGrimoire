7c236be4ad82e1f16c4d67ff9435fffc8f88de83
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-07-11 09:33:02 +0200

    Enhanced error handling for unsupported file types

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -19,10 +19,21 @@ const storage = multer.diskStorage({
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
     if (!extension) {
-      return callback(new Error('Type de fichier non pris en charge.'), null);
+      const error = new Error('Unsupported file type');
+      error.statusCode = 400;
+      return callback(error, null);
     }
     callback(null, name + Date.now() + '.' + extension);
   }
 });
 
-module.exports = multer({ storage }).single('image');
+module.exports = (req, res, next) => {
+  const upload = multer({ storage }).single('image');
+  upload(req, res, (error) => {
+    if (error) {
+      res.status(error.statusCode || 500).json({ error: error.message });
+    } else {
+      next();
+    }
+  });
+};
